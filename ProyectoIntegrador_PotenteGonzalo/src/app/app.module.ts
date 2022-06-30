f466df009906c4353e127ea1bf4ae392aa6279c5
Gonzalo Potente <gonzalopotente2772@gmail.com>
2022-06-30 00:54:34 -0300

    puse banner header y redes sociales

--- ProyectoIntegrador_PotenteGonzalo/src/app/app.module.ts
+++ ProyectoIntegrador_PotenteGonzalo/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgPComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
