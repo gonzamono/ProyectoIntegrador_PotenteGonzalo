8fc596a286cc56f1d586d05533a644ae90d08b8a
Gonzalo Potente <gonzalopotente2772@gmail.com>
2022-06-30 04:11:13 -0300

    cree component acercaDe

--- ProyectoIntegrador_PotenteGonzalo/src/app/app.module.ts
+++ ProyectoIntegrador_PotenteGonzalo/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoArgPComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
